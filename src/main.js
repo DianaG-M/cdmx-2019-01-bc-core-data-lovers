03f632c51c8401243fbae6c349ffbc2ff5e3dffd
Diana <aries_di@hotmail.com>
2019-02-22 15:13:08 -0600

    Se extrajo la base de json y se implemento a todas las funciones

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
-/* //Declaración de variables para manipular la data
-const dataPokemon = window.POKEMON.pokemon;
-
-//Declaración para a secciones del html
+//Declaración para las secciones del html
 const pokemones = document.getElementById('pokemones');
 const theme = document.getElementById('theme');
 
@@ -18,113 +15,120 @@ btnStart.addEventListener("click", () => {
   burguer.classList.remove('hide');
 });
 
-//Función que imprime los datos que son llamados y la imagen de los pokemones en el contenedor pokemones
-const toPrint = (mapData) => {
-  const filter = pokemones;
-  // filter.innerHTML = "";
-  let printPokeons = '';
-  mapData.map((dataPokemon) => {
-    printPokeons +=
-    ` <button type="button" class="divPokemon btnPokemon ${dataPokemon.type[0]} btn btn-primary btn-lg" data-toggle="modal" data-target="#pokemon${dataPokemon.id}">
-        <img src="${dataPokemon.img}"> <br><p class="name">${dataPokemon.name}</p><br>
-      </button>
 
-      <div class="modal fade" id="pokemon${dataPokemon.id}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-        <div class="modal-content bigModal">
+  fetch ('./data/pokemon/pokemon.json')
+  .then(element => element.json())
+  .then(pokemonJson=> {
+    dataPokemon = pokemonJson.pokemon;
+    toPrint(dataPokemon); 
+  })
 
-          <div class="modal-header">
-              <h2 class="modal-title" id="myModalLabel">${dataPokemon.name}</h2>
-              </div>
-            
-            <div class="modal-padd modal-body">
-            <table class="details">
-            <tr> 
-                  <td class="id" >Type:</td>
-                  <td class="dataid">${dataPokemon.type}</td>
-                  <th rowspan="9"><img class="imgModal" src="${dataPokemon.img}"></th>
-                </tr>
 
-                <tr> 
-                  <td class="id">Height:</td>
-                  <td class="dataid">${dataPokemon.height}</td>
-                </tr> 
-
-                <tr>  
-                <td class="id">Weight:</td>
-                  <td class="dataid">${dataPokemon.weight}</td>
-                </tr> 
-                
-                <tr> 
-                  <td class="id">Candy:</td>
-                  <td class="dataid">${dataPokemon.candy}</td>
-                </tr> 
-                
-                <tr> 
-                <td class="id">Candy Count:</td>
-                  <td class="dataid">${dataPokemon.candy_count}</td>
-                  </tr> 
-                
-                <tr> 
-                  <td class="id">Spawn Time:</td>
-                  <td class="dataid">${dataPokemon.spawn_time}</td>
-                </tr> 
-
-                <tr> 
-                  <td class="id">Avg Spawns:</td>
-                  <td class="dataid">${dataPokemon.avg_spawns}</td>
-                </tr> 
-                
-                <tr> 
-                  <td class="id">Egg:</td>
-                  <td class="dataid">${dataPokemon.egg}</td>
-                </tr> 
-                
-                <tr> 
-                  <td class="id">Weaknesses:</td>
-                  <td class="dataid">${dataPokemon.weaknesses}</td>
-                </tr> 
-                
-                <tr>
-                  <th colspan="3">
-                  <table class="evolution">
-                      <tr>
-                        <td><button type="button" class="btn btn-success btn-lg btnxsmallPre btnprueba" data-target="#pokemon${dataPokemon.num}">
-                        <p class="btnEvPre"> <span class="glyphicon glyphicon-backward" aria-hidden="true"></span> PREVIOUS </p>
-                        </button></td>
-
-                        <td><button type="button" class="btn btn-info btn-lg btnxsmallEv btnprueba">
-                        <img class="imgEv" src="${dataPokemon.img}"><br>
-                        <p class="btnEv"> <span class="glyphicon glyphicon-leaf" aria-hidden="true"></span> ACTUAL <span class="glyphicon glyphicon-leaf" aria-hidden="true"></span> </p>
-                        </button></td>
-
-                        <td><button type="button" class="btn btn-success btn-lg btnxsmall btnprueba">
-                        <p class="btnEv"> NEXT <span class="glyphicon glyphicon-forward" aria-hidden="true"></span> </p>
-                        </button></td>
-                        </tr>
-                    </table>
-                    </th>  
-                </tr> 
-                
-                </table>
+  const toPrint = (mapData) => {
+    const filter = pokemones;
+    // filter.innerHTML = "";
+    let printPokeons = '';
+    mapData.map((dataPokemon) => {
+      printPokeons +=
+      ` <button type="button" class="divPokemon btnPokemon ${dataPokemon.type[0]} btn btn-primary btn-lg" data-toggle="modal" data-target="#pokemon${dataPokemon.id}">
+          <img src="${dataPokemon.img}"> <br><p class="name">${dataPokemon.name}</p><br>
+        </button>
+  
+        <div class="modal fade" id="pokemon${dataPokemon.id}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+          <div class="modal-content bigModal">
+  
+            <div class="modal-header">
+                <h2 class="modal-title" id="myModalLabel">${dataPokemon.name}</h2>
                 </div>
-                
-                <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal" aria-label="Close">
-                Close
-              </button>
-              </div>
+              
+              <div class="modal-padd modal-body">
+              <table class="details">
+              <tr> 
+                    <td class="id" >Type:</td>
+                    <td class="dataid">${dataPokemon.type}</td>
+                    <th rowspan="9"><img class="imgModal" src="${dataPokemon.img}"></th>
+                  </tr>
+  
+                  <tr> 
+                    <td class="id">Height:</td>
+                    <td class="dataid">${dataPokemon.height}</td>
+                  </tr> 
+  
+                  <tr>  
+                  <td class="id">Weight:</td>
+                    <td class="dataid">${dataPokemon.weight}</td>
+                  </tr> 
+                  
+                  <tr> 
+                    <td class="id">Candy:</td>
+                    <td class="dataid">${dataPokemon.candy}</td>
+                  </tr> 
+                  
+                  <tr> 
+                  <td class="id">Candy Count:</td>
+                    <td class="dataid">${dataPokemon.candy_count}</td>
+                    </tr> 
+                  
+                  <tr> 
+                    <td class="id">Spawn Time:</td>
+                    <td class="dataid">${dataPokemon.spawn_time}</td>
+                  </tr> 
+  
+                  <tr> 
+                    <td class="id">Avg Spawns:</td>
+                    <td class="dataid">${dataPokemon.avg_spawns}</td>
+                  </tr> 
+                  
+                  <tr> 
+                    <td class="id">Egg:</td>
+                    <td class="dataid">${dataPokemon.egg}</td>
+                  </tr> 
+                  
+                  <tr> 
+                    <td class="id">Weaknesses:</td>
+                    <td class="dataid">${dataPokemon.weaknesses}</td>
+                  </tr> 
+                  
+                  <tr>
+                    <th colspan="3">
+                    <table class="evolution">
+                        <tr>
+                          <td><button type="button" class="btn btn-success btn-lg btnxsmallPre btnprueba" data-target="#pokemon${dataPokemon.num}">
+                          <p class="btnEvPre"> <span class="glyphicon glyphicon-backward" aria-hidden="true"></span> PREVIOUS </p>
+                          </button></td>
+  
+                          <td><button type="button" class="btn btn-info btn-lg btnxsmallEv btnprueba">
+                          <img class="imgEv" src="${dataPokemon.img}"><br>
+                          <p class="btnEv"> <span class="glyphicon glyphicon-leaf" aria-hidden="true"></span> ACTUAL <span class="glyphicon glyphicon-leaf" aria-hidden="true"></span> </p>
+                          </button></td>
+  
+                          <td><button type="button" class="btn btn-success btn-lg btnxsmall btnprueba">
+                          <p class="btnEv"> NEXT <span class="glyphicon glyphicon-forward" aria-hidden="true"></span> </p>
+                          </button></td>
+                          </tr>
+                      </table>
+                      </th>  
+                  </tr> 
+                  
+                  </table>
+                  </div>
+                  
+                  <div class="modal-footer">
+                  <button type="button" class="btn btn-danger" data-dismiss="modal" aria-label="Close">
+                  Close
+                </button>
+                </div>
+  
+            </div>
+            </div>
+        </div>`
+    });
+    filter.innerHTML = printPokeons;
+  }
 
-          </div>
-          </div>
-      </div>`
-  });
-  filter.innerHTML = printPokeons;
-}
 
-toPrint(dataPokemon);
-
-//Filtrado
+  //Filtrado
 const filterType = document.getElementsByClassName('filterType');
 let DataFilter;
 
@@ -150,7 +154,6 @@ for (let i = 0; i < orderName.length; i++) {
   })
 }
 
-
 const stats = document.getElementById('stats');
 const array = [];
 
@@ -159,6 +162,7 @@ stats.addEventListener("click", () => {
     const type = element.type[0];
     array.push(element.avg_spawns);
     const pokeStats = window.data.computeStats(array);
+    console.log(pokeStats)
     pokemones.innerHTML += `
 
     <div class="modal fade" id="pokemon" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
@@ -182,43 +186,12 @@ stats.addEventListener("click", () => {
           </button>
           </div>
           
-          </div>
-          </div>
-          </div>`;
+        </div>
+      </div>
+    </div>`;
           
         });
-      }); */
-
-
-
-//Declaración para las secciones del html
-const pokemones = document.getElementById('pokemones');
-const theme = document.getElementById('theme');
-
-//Declaración para la pantalla de welcome
-const welcome = document.getElementById('welcome');
-const btnStart = document.getElementById('btnStart');
-const burguer = document.getElementById('burguer');
-
-//Boton de inicio
-btnStart.addEventListener("click", () => {
-  welcome.classList.add('hide');
-  pokemones.classList.remove('hide');
-  theme.classList.remove('hide');
-  burguer.classList.remove('hide');
-});
-
-
-  fetch ('http://127.0.0.1:5500/src/data/pokemon/pokemon.json')
-  .then(element => element.json())
-  .then(pokemonJson=> {
-    dataJson = pokemonJson.pokemon;
-    for(let i = 0; i<dataJson.length; i++){
-      let poke = dataJson[i]
-
-      pokemones.innerHTML = `<button>${poke.name}`
-  }
-  })
+      });
 
 
 
