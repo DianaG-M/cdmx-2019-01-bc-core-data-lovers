ae8b2fcb128ca0d287599a382cc7273fe3ccf62a
DianaG-M <aries_di@hotmail.com>
2019-02-23 22:42:11 -0600

    Modificación de la función que hace el calculo

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ window.data = {
   },
 
   sortData: (DataFilter, sortOrder) => {
-    if (sortOrder == "ascendente") {
+    if (sortOrder == "upward") {
       return DataFilter.sort((a, b) => (a.name > b.name) ? 1 : -1);
     } else /* if (sortOrder == "descendente")  */ {
       return DataFilter.sort((a, b) => (a.name < b.name) ? 1 : -1);
@@ -13,11 +13,18 @@ window.data = {
   },
  
 
-  computeStats: (array) => {
-    const result = array.reduce((previuosVal, actualVal ) =>{
-      return previuosVal + actualVal;
-    });
-    const average = (result/array.length).toFixed(2);
-    return average;
+  computeStats: (array, whyIdo) => {
+    if(whyIdo == 'average-type'){
+      const result = array.reduce((previuosVal, actualVal ) =>{
+        return previuosVal + actualVal;
+      });
+      const average = (result/array.length).toFixed(2);
+      return average;
+    }
+    else{
+      console.log('Hola');
+    }
   }
-};
+
+  
+}
